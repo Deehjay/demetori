738908e0a61d98b0c9aa83e3c5025803e1b6a38b
Dan James <41585360+Deehjay@users.noreply.github.com>
2024-11-15 10:24:31 +0000

    initialise project

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <div className="flex justify-center items-center h-screen bg-base-200">
+      <div className="avatar">
+        <div className="w-70 rounded">
+          <img src="https://www.recipetineats.com/tachyon/2014/05/Homemade-Heinz-Baked-Beans_0.jpg?resize=900%2C1260&zoom=1"></img>
+        </div>
+      </div>
+    </div>
+  );
+}
+export default App;
