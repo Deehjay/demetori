0341f70b7897b0a92f9c4cd3f6f5234354cb1f83
Dan James <41585360+Deehjay@users.noreply.github.com>
2024-11-15 10:29:49 +0000

    add alt

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,7 +3,9 @@ function App() {
     <div className="flex justify-center items-center h-screen bg-base-200">
       <div className="avatar">
         <div className="w-70 rounded">
-          <img src="https://www.recipetineats.com/tachyon/2014/05/Homemade-Heinz-Baked-Beans_0.jpg?resize=900%2C1260&zoom=1"></img>
+          <img
+            src="https://www.recipetineats.com/tachyon/2014/05/Homemade-Heinz-Baked-Beans_0.jpg?resize=900%2C1260&zoom=1"
+            alt="Baked Beans for Ben"></img>
         </div>
       </div>
     </div>
